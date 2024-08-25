2d21b28125a8d1c509faa122edf458e1d507c69c
ludic <notetakingforeggs@gmail.com>
2024-08-25 11:40:26 +0100

    Feat: ultra basic rating system (decent y/n) implemented, along with onshore/offshore wind and swell util

--- app/src/main/java/com/example/surfeillance_v2_frontend/service/util/OnshoreOffshore.java
+++ app/src/main/java/com/example/surfeillance_v2_frontend/service/util/OnshoreOffshore.java
@@ -4,21 +4,27 @@ import com.example.surfeillance_v2_frontend.model.entity.ForecastEntity;
 
 public class OnshoreOffshore {
 
-    public Boolean isOffshore(ForecastEntity forecast){
+    public Boolean windIsOffshore(ForecastEntity forecast) {
 
         Double offshoreMinimum = 100.0;
-        Double difference = null;
 
-        if(forecast.getWindDirection() > forecast.getBeachFacing()){
-             difference = forecast.getWaveDirection()-forecast.getBeachFacing();
-        } else if (forecast.getWindDirection() < forecast.getBeachFacing()) {
-            difference = forecast.getBeachFacing() - forecast.getWindDirection();
-        }
-
-        if (difference >= 180 ){
+        Double difference = Math.abs(forecast.getBeachFacing() - forecast.getWindDirection());
+        if (difference >= 180) {
             difference = 360 - difference;
         }
         return difference <= offshoreMinimum;
+    }
+
+    public Boolean wavesAreOnshore(ForecastEntity forecast) {
+
+        Double degreesAwayFromDirectMin = 75D;
+        Double difference = Math.abs(forecast.getWaveDirection() - forecast.getBeachFacing());
+
+        if(difference >= 180){
+            difference = 360 - difference;
+        }
+
+        return difference > degreesAwayFromDirectMin;
     }
 
 }
